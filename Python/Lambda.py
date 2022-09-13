5dbe10015cb8f64df6382c8f1f1fee3d63ac5d2a
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-09-13 16:15:37 +0530

    Update Lambda.py

--- Python/Lambda.py
+++ Python/Lambda.py
@@ -3,7 +3,7 @@ def lambda_handler(event, context):
     # TODO implement
     
     client = boto3.client('sns')
-    snsArn = 'arn:aws:sns:us-east-1:336654342245:ECR_Topic'
+    snsArn = 'arn:aws:sns:us-east-2:547354164162:ECR_Push'
     message = "New Image has been pushed to ECR"
     
     response = client.publish(
