07efbe54b118cafad5c48ae114407bb6a4cf503e
Soumyanil Das <99469217+soumyanild@users.noreply.github.com>
2022-09-13 19:51:32 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -44,14 +44,14 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 data "archive_file" "zip_the_python_code" {
   type = "zip"
   source_dir = "${path.module}/Python"
-  output_path = "${path.module}/Python.zip"
+  output_path = "${path.module}/Python/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
   filename      = "${path.module}/Python/lambda_function.zip"
   function_name = "Nilz-Lambda-Function"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda.lambda_handler"
+  handler       = "lambda_handler.lambda_handler"
   runtime       = "python3.9"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
